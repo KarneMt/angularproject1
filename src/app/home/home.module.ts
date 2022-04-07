f6b32ab099e93ebea47dbe52501f3a1b977f1ea5
Kai Arne <kaiarne.moebert@outlook.de>
2022-04-07 16:02:37 +0200

    Home hinzugefügt

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
